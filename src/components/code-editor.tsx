df48b42107c4b9fea5a9e2d13c40fb6be8c34ba8
bo98117 <bosin98117@gmail.com>
2023-12-12 09:07:28 -0800

    update

--- src/components/code-editor.tsx
+++ src/components/code-editor.tsx
@@ -1,23 +1,68 @@
-import MonacoEditor from '@monaco-editor/react';
+import './code-editor.css';
+import { useRef } from 'react';
+import MonacoEditor, { EditorDidMount } from '@monaco-editor/react';
+import prettier from 'prettier';
+import parser from 'prettier/parser-babel';
 
 
-const CodeEditor = () => {
+interface CodeEditorProps {
+    initialValue: string;
+    onChange(value: string): void;
+}
 
-    return <MonacoEditor
-        theme="dark"
-        language='javascript'
-        height="300px"
-        options={{
-            wordWrap: 'on',
-            minimap: { enabled: false },
-            showUnused: false,
-            folding: false,
-            lineNumbersMinChars: 3,
-            fontSize: 16,
-            scrollBeyondLastLine: false,
-            automaticLayout: true,
-        }}
-    />;
+const CodeEditor: React.FC<CodeEditorProps> = ({ onChange, initialValue }) => {
+    const editorRef = useRef<any>();
+
+    const onEditorDidMount: EditorDidMount = (getValue, monacoEditor) => {
+        editorRef.current = monacoEditor;
+        monacoEditor.onDidChangeModelContent(() => {
+            onChange(getValue());
+        });
+
+        monacoEditor.getModel()?.updateOptions({ tabSize: 2 });
+    }
+
+    const onFormatClick = () => {
+        console.log(editorRef.current);
+        // Get current value from editor
+        const unformatted = editorRef.current.getModel().getValue();
+
+        // Format that value
+        const formatted = prettier.format(unformatted, {
+            parser: 'babel',
+            plugins: [parser],
+            useTabs: false,
+            semi: true,
+            singleQuote: true
+        }).replace(/\n$/, '');
+
+        //Set the formatted value back in the editor
+        editorRef.current.setValue(formatted);
+    }
+
+
+    return (
+        <div className='editor-wrapper'>
+            <button className='button button-format is-primary is-small' onClick={onFormatClick}>Fromat</button>
+            <MonacoEditor
+                editorDidMount={onEditorDidMount}
+                value={initialValue}
+                theme="dark"
+                language='javascript'
+                height="300px"
+                options={{
+                    wordWrap: 'on',
+                    minimap: { enabled: false },
+                    showUnused: false,
+                    folding: false,
+                    lineNumbersMinChars: 3,
+                    fontSize: 16,
+                    scrollBeyondLastLine: false,
+                    automaticLayout: true,
+                }}
+            />
+        </div>
+    )
 }
 
 export default CodeEditor;
---
+++ src/components/preview.tsx
@@ -0,0 +1,46 @@
+import { useEffect, useRef } from "react";
+
+
+interface PreviewProps {
+    code: string;
+}
+
+const html = `
+    <html>
+        <head></head>
+        <body>
+            <div id="root"></div>
+            <script>
+                window.addEventListener('message', (event) => {
+                    try {
+                        eval(event.data);
+                    } catch (err) {
+                        const root = document.querySelector('#root');
+                        root.innerHTML = '<dvi style="color: red;"><h4>Runtime Error></h4>' + err + '</div>';
+                        console.error(err);
+                    }
+                }, false);
+            </script>
+        </body>
+    </html>
+    `
+
+const Preview: React.FC<PreviewProps> = ({ code }) => {
+    const iframe = useRef<any>();
+
+    useEffect(() => {
+        iframe.current.srcdoc = html;
+
+        // setCode(result.outputFiles[0].text);
+        iframe.current.contentWindow.postMessage(code, '*');
+    }, [code]);
+
+    return <iframe
+        title="preview"
+        ref={iframe}
+        sandbox='allow-scripts'
+        srcDoc={html}
+    />;
+}
+
+export default Preview;
