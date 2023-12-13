c443ee6dfcc5bca7de3cc01169bf85c47ee5e1f9
bo98117 <bosin98117@gmail.com>
2023-12-13 09:01:13 -0800

    update

--- src/components/preview.tsx
+++ src/components/preview.tsx
@@ -1,23 +1,35 @@
+import './preview.css';
 import { useEffect, useRef } from "react";
 
 
 interface PreviewProps {
     code: string;
+    err: string;
 }
 
 const html = `
     <html>
-        <head></head>
+        <head>
+            <style>html { background-color: white; }</style>
+        </head>
         <body>
             <div id="root"></div>
             <script>
+                const handleError = (err) => {
+                    const root = document.querySelector('#root');
+                    root.innerHTML = '<dvi style="color: red;"><h4>Runtime Error></h4>' + err + '</div>';
+                    console.error(err);
+                };
+                window.addEventListener('error', (event) => {
+                    event.preventDefault();
+                    handleError(event.error);
+                });
+
                 window.addEventListener('message', (event) => {
                     try {
                         eval(event.data);
                     } catch (err) {
-                        const root = document.querySelector('#root');
-                        root.innerHTML = '<dvi style="color: red;"><h4>Runtime Error></h4>' + err + '</div>';
-                        console.error(err);
+                        handleError(err);
                     }
                 }, false);
             </script>
@@ -25,22 +37,29 @@ const html = `
     </html>
     `
 
-const Preview: React.FC<PreviewProps> = ({ code }) => {
+const Preview: React.FC<PreviewProps> = ({ code, err }) => {
     const iframe = useRef<any>();
 
     useEffect(() => {
         iframe.current.srcdoc = html;
-
-        // setCode(result.outputFiles[0].text);
-        iframe.current.contentWindow.postMessage(code, '*');
+        setTimeout(() => {
+            // setCode(result.outputFiles[0].text);
+            iframe.current.contentWindow.postMessage(code, '*');
+        }, 50);
     }, [code]);
 
-    return <iframe
-        title="preview"
-        ref={iframe}
-        sandbox='allow-scripts'
-        srcDoc={html}
-    />;
+
+    return (
+        <div className="preview-wrapper">
+            <iframe
+                title="preview"
+                ref={iframe}
+                sandbox='allow-scripts'
+                srcDoc={html}
+            />
+            {err && <div className='preview-error'>{err}</div>}
+        </div>
+    )
 }
 
 export default Preview;
--- src/components/resizable.tsx
+++ src/components/resizable.tsx
@@ -1,4 +1,6 @@
-import { ResizableBox } from "react-resizable";
+import './resizable.css';
+import { useEffect, useState } from 'react';
+import { ResizableBox, ResizableBoxProps } from "react-resizable";
 
 
 interface ResizableProps {
@@ -7,9 +9,59 @@ interface ResizableProps {
 }
 
 const Resizable: React.FC<ResizableProps> = ({ direction, children }) => {
+    let resizableProps: ResizableBoxProps;
+
+    const [innerHeight, setInnerHeight] = useState(window.innerHeight);
+    const [innerWidth, setInnerWidth] = useState(window.innerWidth);
+    const [width, setWidth] = useState(window.innerWidth * 0.75);
+
+    useEffect(() => {
+        let timer: any;
+        const listener = () => {
+            if (timer) {
+                clearTimeout(timer);
+            }
+            timer = setTimeout(() => {
+                setInnerHeight(window.innerHeight);
+                setInnerWidth(window.innerWidth);
+                if (window.innerWidth * 0.75 < width) {
+                    setWidth(window.innerWidth * 0.75);
+                }
+            }, 100);
+        }
+        window.addEventListener('resize', listener);
+
+        return () => {
+            window.removeEventListener('resize', listener);
+        }
+    }, [])
+
+    if(direction === 'horizontal') {
+        resizableProps = {
+            className: 'resize-horizontal',
+            minConstraints: [innerWidth * 0.2, Infinity],
+            maxConstraints: [innerWidth * 0.75, Infinity],
+            height: Infinity,
+            width: width,
+            resizeHandles: ['e'],
+            onResizeStop: (event, data) => {
+                setWidth(data.size.width);
+            },
+        }
+    } else {
+        resizableProps = {
+            minConstraints: [Infinity, 24],
+            maxConstraints: [Infinity, innerHeight * 0.9],
+            height: 300,
+            width: Infinity,
+            resizeHandles: ['s'],
+        }
+    }
 
     return (
-        <ResizableBox height={300} width={300}>{children}</ResizableBox>
+        <ResizableBox {...resizableProps}>
+            {children}
+        </ResizableBox>
     )
 }
 
